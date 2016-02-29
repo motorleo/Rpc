06501d4b3d01a8f81b49cdc74148987fd9204a37
motorleo <lee3239@outlook.com>
2016-02-29 23:05:05 +0800

    add

--- test/client_test.cc
+++ test/client_test.cc
@@ -2,9 +2,10 @@
 #include "../src/RpcClientLite.h"
 #include <muduo/base/Logging.h>
 #include <muduo/net/EventLoop.h>
+#include <muduo/net/EventLoopThreadPool.h>
+#include <boost/bind.hpp>
 
 #include <stdio.h>
-#include <time.h>
 #include <vector>
 #include <string>
 
@@ -24,20 +25,23 @@ public:
         :RpcClientLite(loop,addr),
 		stub_(getChannel()),
 		messagecount_(0),
-		owner_(owner)
+		owner_(owner),
+		exit_(false)
     {
     }
 
 	int messagecount() { return messagecount_;}
+
+	void setExit() { exit_ = true; }
 private:
     virtual void initCallback(const TcpConnectionPtr& conn);
     
     void doneCallback(test::testMessage* request);
 
     test::testService_Stub stub_;
-	time_t startTime_;
 	int messagecount_;
 	bench* owner_;
+	bool exit_;
 };
 
 class bench
@@ -48,20 +52,32 @@ public:
 			int nClient,
 			int nMessageSize,
 			int Timeout)
-		:loop_(loop),testVec_(),string_(),ClientCount_(0),timeout_(Timeout)
+		:loop_(loop),threadPool_(loop,""),ClientCount_(0),timeout_(Timeout)
 	{
+		loop->runAfter(Timeout, boost::bind(&bench::setExit, this));
 		for (int i = 0;i < nMessageSize;++i)
 		{
 			string_ += 'i';
 		}
 
-		while (++ClientCount_ <= nClient)
+		//threadPool_.setThreadNum(2);
+		threadPool_.start();
+
+		for (int i = 0;i < nClient;++i)
 		{
-			testClient* client = new testClient(loop,addr,this);
+			testClient* client = new testClient(threadPool_.getNextLoop(),addr,this);
 			client->connect();
 			testVec_.push_back(client);
 		}
-		--ClientCount_;
+	}
+
+	void setExit()
+	{
+		std::vector<testClient*>::iterator iter = testVec_.begin();
+		for (;iter < testVec_.end();++iter)
+		{
+			(*iter)->setExit();
+		}
 	}
 
 	int getTimeout() { return timeout_;}
@@ -88,8 +104,10 @@ public:
 		}
 	}
 
+	void increaseCount() { ++ClientCount_; }
 private:
 	EventLoop* loop_;
+	EventLoopThreadPool threadPool_;
 	std::vector<testClient*> testVec_;
 	std::string string_;
 	int ClientCount_;
@@ -98,17 +116,17 @@ private:
 
 void testClient::initCallback(const TcpConnectionPtr& conn)
 {   
+	owner_->increaseCount();
 	test::testMessage request;
 	request.set_echostring(owner_->getString());
 	test::testMessage* response(new test::testMessage());
 	stub_.Echo(NULL,&request,response,NewCallback(this,&testClient::doneCallback,response));
-	startTime_ = ::time(NULL);
 }
 
 void testClient::doneCallback(test::testMessage* request)
 {   
 	++messagecount_;
-	if (time(NULL) - startTime_ < owner_->getTimeout())
+	if (!exit_)
 	{
 		if (request)
 		{
@@ -128,6 +146,7 @@ int main(int argc,char** argv)
 {
 	if (argc >= 5)
 	{
+		Logger::setLogLevel(Logger::WARN);
 		EventLoop loop;
 		InetAddress address(argv[1],9981);
 		int nClient = atoi(argv[2]);
